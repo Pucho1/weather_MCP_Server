ea4a3390b8433f281019e263289a187b20b764d3
Pucho <martinezochandarenam@gmail.com>
2026-05-09 18:41:53 +0200

    feat: change block IO move stdin to another thread

--- weather.py
+++ weather.py
@@ -1,14 +1,15 @@
 import sys
 import json
-
+import asyncio # concurrencia cooperativa lib de python
 
 
 async def get_weather(params):
     city = params.get("city")
 
-    if city  == "crash":
-        1 / 0  # Simula un error dividiendo por cero
-
+    if city  == "Madrid":
+        # SIMULA una operación asíncrona, no detiene el programa sino que cede el hilo a otra tarea
+        await asyncio.sleep(5)  
+    
     return {
         "city": city,
         "temperature": "22C"
@@ -18,8 +19,7 @@ methods = {
     "weather/get": get_weather
 }
 
-# Escucha mensajes entrantes
-for line in sys.stdin:
+async def  handle_request(line):
     request = json.loads(line) # Parsea el mensaje recibido como JSON
 
     method_name = request.get("method")
@@ -27,23 +27,10 @@ for line in sys.stdin:
     request_id = request.get("id")
 
 
-    #  Verifica si el método solicitado existe en el diccionario de métodos
-    if method_name not in methods:
-        error_response = {
-            "jsonrpc": "2.0",
-            "id": request_id,
-            "error": {
-                "code": -32601,
-                "message": "Method not found"
-            }
-        }
-
-        print(json.dumps(error_response), flush=True)
-        continue
-
-    # Ejecuta el método y maneja cualquier error que pueda ocurrir 
+    # Captura exepcones globales garantizando una respuesta el cliente
     try:
-        result = methods[method_name](params)
+        # Ejecuta el método correspondiente con los parámetros proporcionados y espera su resultado sin bloquear el loop
+        result = await methods[method_name](params)
 
         response = {
             "jsonrpc": "2.0",
@@ -60,6 +47,20 @@ for line in sys.stdin:
                 "message": str(e)
             }
         }
-
-    # Responde por stdout sin flush:true el clinte puede buferisarce y no recibir la respuesta inmediatamente
     print(json.dumps(response), flush=True)
+    
+
+async def main():
+
+    while True:
+        line = await asyncio.to_thread(sys.stdin.readline)
+
+        if not line:
+            break
+
+        asyncio.create_task(handle_request(line))
+    
+
+
+# Crea event loop, ejecuta main, destruye loop al terminar
+asyncio.run(main())
